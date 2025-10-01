ea845087b61cb890c452692f49e13ffc7d6a31d3
AlperenAltngl <154558307+AlperenAltngl@users.noreply.github.com>
2025-10-01 22:23:04 +0300

    first commit

---
+++ week2/app/src/main/java/com/example/myapplication/Animal.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+public class Animal {
+    private String type;
+    private Integer picId;
+
+    public Animal(String type, Integer picId) {
+        this.type = type;
+        this.picId = picId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getPicId() {
+        return picId;
+    }
+
+    public void setPicId(Integer picId) {
+        this.picId = picId;
+    }
+}
---
+++ week2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnArrayAdapter;
+    Button btnCustomAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnArrayAdapter = (Button) findViewById(R.id.button3);
+        btnArrayAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ArrayAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCustomAdapter = (Button) findViewById(R.id.button4);
+        btnCustomAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
